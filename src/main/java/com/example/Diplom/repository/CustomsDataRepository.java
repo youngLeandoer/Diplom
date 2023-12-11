af3b243b8c82967f77c6b666a813991a55d1f0f6
youngLeandoer <99642816+youngLeandoer@users.noreply.github.com>
2023-12-11 23:46:31 +0300

    Update CustomsDataRepository.java

--- src/main/java/com/example/Diplom/repository/CustomsDataRepository.java
+++ src/main/java/com/example/Diplom/repository/CustomsDataRepository.java
@@ -4,5 +4,4 @@ import com.example.Diplom.model.CustomsData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomsDataRepository extends JpaRepository<CustomsData, Long> {
-    // Дополнительные методы, если необходимо
 }
